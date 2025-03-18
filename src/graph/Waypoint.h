9b1296323b452b2ad0b453354127ddaaf4437dd3
piotrekkow <138975303+piotrekkow@users.noreply.github.com>
2025-03-18 09:12:20 +0100

    Update Waypoint.h

--- src/graph/Waypoint.h
+++ src/graph/Waypoint.h
@@ -3,10 +3,13 @@
 class Waypoint :
     public Vertex
 {
-    Edge* m_in;
-    Edge* m_out;
+    Edge* m_in{ nullptr };
+    Edge* m_out{ nullptr };
 
 public:
+	Waypoint(Vector2 position);
     Edge* in() const;
     Edge* out() const;
+    bool addIn(Edge* inEdge) override;
+    bool addOut(Edge* outEdge) override;
 };
