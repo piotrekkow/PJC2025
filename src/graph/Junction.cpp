9e2f63d1136e226455049dde9b8b344e97e2a927
piotrekkow <138975303+piotrekkow@users.noreply.github.com>
2025-03-18 11:09:13 +0100

    Update Junction.cpp

--- src/graph/Junction.cpp
+++ src/graph/Junction.cpp
@@ -1,1 +1,25 @@
 #include "Junction.h"
+
+Junction::Waypoint(Vector2 position) : Vertex(position) {}
+
+std::vector<Edge*> Junction::in() const
+{
+	return m_in;
+}
+
+std::vector<Edge*> Junction::out() const
+{
+	return m_out;
+}
+
+bool Waypoint::addIn(Edge* inEdge)
+{
+	m_in.push_back(inEdge);
+	return true;
+}
+
+bool Waypoint::addOut(Edge* outEdge)
+{
+	m_out.push_back(outEdge);
+	return true;
+}
