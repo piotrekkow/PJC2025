05998873d39e8a48f9cc7cbd781517b196fb4276
piotrekkow <138975303+piotrekkow@users.noreply.github.com>
2025-03-18 09:22:13 +0100

    Update Waypoint.cpp

--- src/graph/Waypoint.cpp
+++ src/graph/Waypoint.cpp
@@ -1,5 +1,7 @@
 #include "Waypoint.h"
 
+Waypoint::Waypoint(Vector2 position) : Vertex(position) {}
+
 Edge* Waypoint::in() const
 {
 	return m_in;
@@ -8,4 +10,24 @@ Edge* Waypoint::in() const
 Edge* Waypoint::out() const
 {
 	return m_out;
+}
+
+bool Waypoint::addIn(Edge* inEdge)
+{
+	if (!m_in)
+	{
+		m_in = inEdge;
+		return true;
+	}
+	return false;
+}
+
+bool Waypoint::addOut(Edge* outEdge)
+{
+	if (!m_out)
+	{
+		m_out = outEdge;
+		return true;
+	}
+	return false;
 }
